eb73967658440c24d3341fbc718e720849135c0f
王超 <597584896@qq.com>
2022-08-12 14:42:33 +0800

    feat: ✨优先队列

---
+++ PriorityQueue.js
@@ -0,0 +1,11 @@
+/*
+ * @Description: 优先队列，放入队列的每个元素，根据优先值进行排序，可获取最优先的元素
+ * @Author: wangchao
+ * @Date: 2022-08-12 14:40:05
+ */
+class PriorityQueue {
+  constructor(arr){
+    this.arr = arr
+  }
+  
+}
